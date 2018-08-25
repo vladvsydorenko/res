8feb21c58338469799d8700504deefea73ab950c
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2018-08-26 02:40:01 +0300

    Rewrited, but it's not last time unfortunetally

--- src/NodeManager.ts
+++ src/NodeManager.ts
@@ -14,21 +14,23 @@ export class NodeManager<T> {
     // all set transforms
     private transforms: THash<TTransform<T>> = {};
 
+    // applied to connections transforms, targetNodeId => transformId[]
+    private nodeToTransforms: THash<TId[]> = {};
+
     // source => targets
     private sourceToTargetsConnections: THash<TId[]> = {};
 
     // target => sources
     private targetToSourcesConnections: THash<TId[]> = {};
 
-    // applied to connections transforms, targetNodeId => transformId
-    private appliedTransforms: THash<TId> = {};
+    // transformId => targetNodeId
+    private transformToNodes: THash<TId[]> = {};
 
     public setNode(node: INode<T>, source: INode<T> = node) {
-        const transformId = this.appliedTransforms[<string>node.id];
-        const transform = this.transforms[<string>transformId];
+        const transforms = this.nodeToTransforms[<string>node.id];
         const oldNode = this.nodes[<string>node.id] || node;
 
-        const newNode = transform ? transform(node, oldNode, source, this) : node;
+        const newNode = transforms ? this.applyTransforms(node, oldNode, source, transforms) : node;
         this.nodes[<string>newNode.id] = newNode;
 
         const targetNodeIds = this.sourceToTargetsConnections[<string>newNode.id];
@@ -56,12 +58,41 @@ export class NodeManager<T> {
         });
     }
 
-    public setTransform(transformId: TId, transform: TTransform<T>) {
+    public setNodeTransforms(nodeId: TId, transformIds: TId[]) {
+        this.nodeToTransforms[<string>nodeId] = transformIds;
+        const node = this.nodes[<string>nodeId];
+
+        transformIds.forEach(transformId => insertInHashOfArrays(nodeId, transformId, this.transformToNodes));
+
+        if (!node) return;
+        this.setNode(node);
+    }
+
+    public addTransform(transformId: TId, transform: TTransform<T>) {
         this.transforms[<string>transformId] = transform;
+        const targetNodeIds = this.transformToNodes[<string>transformId];
+
+        if (!targetNodeIds) return;
+
+        targetNodeIds.forEach(targetNodeId => {
+            const node = this.nodes[<string>targetNodeId];
+
+            if (!node) return;
+
+            this.setNode(node);
+        });
     }
 
-    public applyTransform(transformId: TId, nodeId: TId) {
-        this.appliedTransforms[<string>nodeId] = transformId;
-    }
+    private applyTransforms(node: INode<T>, oldNode: INode<T>, source: INode<T>, transformIds: TId[]) {
+        let old = oldNode;
+        return transformIds.reduce((current, transformId) => {
+            const transform = this.transforms[<string>transformId];
 
+            if (!transform) return current;
+
+            old = transform(current, old, source, this);
+
+            return old;
+        }, node);
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,28 @@ import { NodeManager } from "./NodeManager";
 const nm = new NodeManager<any[]>();
 
 nm.setNode({
-    id: "entities",
+    id: "numbers",
     value: [1, 2, 3],
 });
 
-nm.setNodeConnection("entities", "users");
+nm.setNodeConnection("numbers", "even");
+nm.setNodeConnection("numbers", "odd");
+
+nm.setNodeTransforms("even", ["even"]);
+nm.setNodeTransforms("odd", ["odd"]);
+
+nm.addTransform("even", (node) => {
+    return {
+        id: node.id,
+        value: node.value.filter(v => v % 2),
+    };
+});
+
+nm.addTransform("odd", (node) => {
+    return {
+        id: node.id,
+        value: node.value.filter(v => v % 2 === 0),
+    };
+});
 
 console.log(nm);
