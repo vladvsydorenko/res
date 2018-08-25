16c6e860eda90c424130690c323c86079be7e835
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2018-08-25 22:44:27 +0300

    Yes, this is what I mean. Let me do last rewerite then

--- src/NodeManager.ts
+++ src/NodeManager.ts
@@ -1,20 +1,33 @@
-export type TNodeSetter<T> = (
+import { EventEmitter, TListenerFn } from "./EventEmitter";
+
+export type TTransfrom<T> = (
     newNode: Readonly<INode<T>>,
-    oldNode: Readonly<INode<T>>,
-    parentNode: Readonly<INode<T>> | null,
+    oldNode: Readonly<INode<T>> | undefined,
+    parentNode: Readonly<INode<T>> | undefined,
     manager: Readonly<NodeManager<T>>
 ) => INode<T>;
 
 export interface INode<T> {
-    readonly id: string | symbol;
+    readonly id: TNodeId;
     readonly value: T;
 }
+
+export enum ENodeManagerEvents {
+    set = "set",
+}
+
+export type TNodeId = string | symbol;
 
 export class NodeManager<T = any[]> {
 
     private nodes: { [nodeId: string]: INode<T> } = {};
-    private transforms: { [nodeId: string]: TNodeSetter<T> } = {};
-    private connections: { [nodeId: string]: (string | symbol)[] } = {};
+    private transforms: { [nodeId: string]: TTransfrom<T> } = {};
+    private sourceToTargetConnections: { [nodeId: string]: TNodeId[] } = {};
+    private targetToSourceConnections: { [nodeId: string]: TNodeId[] } = {};
+
+    private eventEmitter = new EventEmitter();
+
+    // --- nodes
 
     public setNode(node: INode<T>, parent?: INode<T>): INode<T> {
         const nodeId = <string>node.id;
@@ -24,34 +37,86 @@ export class NodeManager<T = any[]> {
         const resultNode = transform ? transform(node, oldNode, parent, this) : node;
         this.nodes[nodeId] = resultNode;
 
+        this.eventEmitter.emit(ENodeManagerEvents.set, [node, parent, this]);
+
         this.updateConnectedNodes(resultNode);
 
         return resultNode;
     }
 
-    public getNode(nodeId: string | symbol) {
+    public setManyNodes(nodes: INode<T>[]) {
+        nodes.forEach(node => this.setNode(node));
+    }
+
+    public getNode(nodeId: TNodeId): Readonly<INode<T>> {
         return this.nodes[<string>nodeId];
     }
 
-    public setTransform(nodeId: string | symbol, setter: TNodeSetter<T>) {
+    // --- transform
+
+    public setTransform(nodeId: TNodeId, setter: TTransfrom<T>) {
         this.transforms[<string>nodeId] = setter;
     }
 
-    public setConnection(parentNodeId: string | symbol, childNodeId: string | symbol) {
-        const prevConnections = this.connections[<string>parentNodeId] || [];
+    // --- connections
 
-        if (prevConnections && prevConnections.indexOf(childNodeId) !== -1) return;
+    public connectNodes(sourceNodeId: TNodeId, targetNodeId: TNodeId) {
+        const prevTargets = this.sourceToTargetConnections[<string>sourceNodeId] || [];
+        const prevSources = this.targetToSourceConnections[<string>targetNodeId] || [];
 
-        this.connections[<string>parentNodeId] = [
-            ...(prevConnections ? prevConnections : []),
-            childNodeId,
+        if (prevTargets && prevTargets.indexOf(targetNodeId) !== -1) return;
+
+        this.sourceToTargetConnections[<string>sourceNodeId] = [
+            ...(prevTargets ? prevTargets : []),
+            targetNodeId,
+        ];
+
+        this.targetToSourceConnections[<string>targetNodeId] = [
+            ...(prevSources ? prevSources : []),
+            sourceNodeId,
         ];
     }
 
-    private updateConnectedNodes(node: INode<T>) {
-        const nodeConnections = this.connections[<string>node.id];
+    public connectManyNodes(nodeIds: TNodeId[]) {
+        nodeIds.forEach((childId, index) => {
+            // there is no parent for 0 elemtn
+            if (index === 0) return;
 
-        console.log('update children?', node.id);
+            const parentId = nodeIds[index - 1];
+            this.connectNodes(parentId, childId);
+        });
+    }
+
+    public getSourceNodeIds(targetNodeId: TNodeId): ReadonlyArray<TNodeId> {
+        return this.targetToSourceConnections[<string>targetNodeId] || [];
+    }
+
+    public getSourceNodes(targetNodeId: TNodeId): ReadonlyArray<INode<T>> {
+        return this.getSourceNodeIds(targetNodeId).map(sourceId => this.nodes[<string>sourceId]);
+    }
+
+    public getTargetNodeIds(sourceNodeId: TNodeId): ReadonlyArray<TNodeId> {
+        return this.sourceToTargetConnections[<string>sourceNodeId] || [];
+    }
+
+    public getTargetNodes(sourceNodeId: TNodeId): ReadonlyArray<INode<T>> {
+        return this.getTargetNodeIds(sourceNodeId).map(targetNodeId => this.nodes[<string>targetNodeId]);
+    }
+
+    // --- events
+
+    public on(eventId: ENodeManagerEvents, listener: TListenerFn, thisArg?: any) {
+        return this.eventEmitter.on(eventId, listener, thisArg);
+    }
+
+    public off(listenerId: symbol) {
+        this.eventEmitter.off(listenerId);
+    }
+
+    // --- helpers
+
+    private updateConnectedNodes(node: INode<T>) {
+        const nodeConnections = this.sourceToTargetConnections[<string>node.id];
 
         if (!nodeConnections) return;
 
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
-import { NodeManager } from "./NodeManager";
+import { NodeManager, ENodeManagerEvents } from "./NodeManager";
 
 const nm = new NodeManager<number[]>();
 
-// nm.setNode({
-//     id: "evenNumbers",
-//     value: []
-// });
+nm.connectNodes("entities", "evenNumbers");
+nm.connectNodes("entities", "greaterThen10");
+nm.connectManyNodes(["entities", "evenNumbers", "greaterThen10"]);
 
-nm.setConnection("entities", "evenNumber");
-
-nm.setNode({
-    id: "entities",
-    value: [1, 2, 3],
+nm.setTransform("evenNumbers", node => {
+    return {
+        id: node.id,
+        value: node.value.filter(v => v % 2),
+    };
 });
 
-console.log(nm);
+nm.setTransform("greaterThen10", node => {
+    return {
+        id: node.id,
+        value: node.value.filter(v => v > 10),
+    };
+});
+
+nm.on(ENodeManagerEvents.set, (node, parent, nm) => {
+    // console.log("set", node, parent, nm);
+});
+
+nm.setManyNodes([{
+    id: "entities",
+    value: [1, 2, 3, 20],
+}]);
+
+console.log(nm.getTargetNodes("entities"));
