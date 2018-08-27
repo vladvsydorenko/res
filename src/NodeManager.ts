0951d4c6f1d15df9da92d9de4dd8d8ace8978f02
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2018-08-27 15:24:00 +0300

    up

--- src/NodeManager.ts
+++ src/NodeManager.ts
@@ -1,6 +1,7 @@
 import { RelationManager } from "./RelationManager";
 import { TId, THash } from "./types";
 import { WeakRelationManager } from "./WeakRelationManager";
+import { EventEmitter } from "./EventEmitter";
 
 export interface INode<T> {
     id: TId;
@@ -14,12 +15,25 @@ export type TTransform<T> = (
     nodeManager: NodeManager<T>
 ) => T;
 
+export interface INodeManagerBulkOptions<T> {
+    setNode?: (INode<T> | [INode<T>, INode<T>])[];
+    setNodeTargets?: [TId, TId[]];
+    setNodeSources?: [TId, TId[]];
+    setNodeTransforms?: [TId, TTransform<T>[]];
+    setTransformNodes?: [TTransform<T>, TId[]];
+    bulk?: INodeManagerBulkOptions<T>[];
+}
+
 export class NodeManager<T> {
 
     private nodes: THash<INode<T>> = {};
 
     private nodeConnections = new RelationManager();
     private transformConnections = new WeakRelationManager<TTransform<T>>();
+
+    private eventEmitter = new EventEmitter();
+
+    private isReactiveEnabled = true;
 
     // -----------------------------------------------------------------------
     // Nodes
@@ -30,6 +44,8 @@ export class NodeManager<T> {
         const targetNodeIds = this.nodeConnections.getTargets(node.id);
 
         this.nodes[<string>node.id] = newNode;
+
+        this.eventEmitter.emit("set", [newNode, source]);
 
         targetNodeIds.forEach(targetNodeId => {
             this.setNode({
@@ -66,6 +82,7 @@ export class NodeManager<T> {
 
     public setNodeTargets(nodeId: TId, targetNodeIds: TId[]) {
         this.nodeConnections.setTargets(nodeId, targetNodeIds);
+        targetNodeIds.forEach(nodeId => this.recalculateNode(nodeId));
     }
 
     public getNodeTargets(nodeId: TId) {
@@ -74,10 +91,11 @@ export class NodeManager<T> {
 
     public setNodeSources(nodeId: TId, sourceNodeIds: TId[]) {
         this.nodeConnections.setSources(nodeId, sourceNodeIds);
+        this.recalculateNode(nodeId);
     }
 
     public getNodeSources(nodeId: TId) {
-        return this.nodeConnections.sources[<string>nodeId];
+        return this.nodeConnections.getSources(nodeId);
     }
 
     // -----------------------------------------------------------------------
@@ -86,18 +104,56 @@ export class NodeManager<T> {
 
     public setNodeTransforms(nodeId: TId, transforms: TTransform<T>[]) {
         this.transformConnections.setSources(nodeId, transforms);
-    }
-
-    public setTransformNodes(transformId: TTransform<T>, nodeIds: TId[]) {
-        this.transformConnections.setTargets(transformId, nodeIds);
+        this.recalculateNode(nodeId);
     }
 
     public getNodeTransforms(nodeId: TId) {
         return this.transformConnections.getSources(nodeId);
     }
 
+    public setTransformNodes(transformId: TTransform<T>, nodeIds: TId[]) {
+        this.transformConnections.setTargets(transformId, nodeIds);
+        nodeIds.forEach(nodeId => this.recalculateNode(nodeId));
+    }
+
     public getTransformNodes(transformId: TTransform<T>) {
         return this.transformConnections.getTargets(transformId);
+    }
+
+    public bulk(options: INodeManagerBulkOptions<T>) {
+        if (options.setNode) {
+            options.setNode.forEach(data => {
+
+            });
+        }
+    }
+
+    public recalculateNode(nodeId: TId) {
+        if (!this.isReactiveEnabled) return;
+
+        const node = this.nodes[<string>nodeId];
+        if (!node) return;
+
+        const sources = this.getNodeSources(nodeId);
+
+        if (sources.length === 0) {
+            this.setNode(node);
+            return;
+        }
+
+        sources.forEach(sourceId => {
+            this.setNode(node, this.nodes[<string>sourceId]);
+        });
+    }
+
+    public recalculateAllNodes() {
+        Object
+            .keys(this.nodes)
+            .forEach(nodeId => {
+                const sources = this.getNodeSources(nodeId);
+                // we want update only top-level (having no sources) nodes
+                if (sources.length === 0) this.setNode(this.nodes[nodeId])
+            });
     }
 
 }
